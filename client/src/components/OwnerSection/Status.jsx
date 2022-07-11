3eecd7ffb34ac9f566807e760407f9025b11ed3b
Jean <jgatbkk@gmail.com>
2022-07-11 16:04:03 +0200

    added switch case for session status

--- client/src/components/OwnerSection/Status.jsx
+++ client/src/components/OwnerSection/Status.jsx
@@ -1,9 +1,29 @@
 function Status({value}) {
-    return(
-      <span>
-        <strong>{value}</strong>
-      </span>
-    )
+  
+  const getStatus = (status) => {
+    switch(status){
+      case "0":
+        return "Registering Voters"
+      case "1": 
+        return "Proposals registration has started"
+      case "2":
+        return "Proposals registration has ended"
+      case "3":
+        return "Voting session has started"
+      case "4":
+        return "Voting session has ended"
+      case "5":
+        return "Votes Tallied"
+      default:
+        return "Registering Voters"
+    }
+  }
+  
+  return(
+    <span>
+      <strong>{getStatus(value)}</strong>
+    </span>
+  )
 }
 
 export default Status;
