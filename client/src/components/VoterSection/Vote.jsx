42b7f64d7543e1a7199e6c58ca5ff0046b8de5b8
Jean <jgatbkk@gmail.com>
2022-07-12 15:08:12 +0200

    new voter event, changed to call() for get methods

--- client/src/components/VoterSection/Vote.jsx
+++ client/src/components/VoterSection/Vote.jsx
@@ -5,35 +5,38 @@ function Vote() {
     const { state: { contract, accounts } } = useEth();
     const [proposaId, updateProposalId] = useState();
     const [voterAddress, updateVoterAddress] = useState("");
+    const [votedId, updateVotedId] = useState();
 
     const vote = async() => {
-        await contract.methods.setVote(proposaId).send({ from: accounts[0] });
+        if (/^(0|[1-9][0-9]*)$/.test(proposaId)) {
+            await contract.methods.setVote(proposaId).send({ from: accounts[0] });
+        }
     }
-
+        
     const getVoter = async() => {
-        const transac = await contract.methods.getVoter(voterAddress).send({ from: accounts[0] });
-        console.log(transac); // show voter info
+        const addr = document.getElementById("address").value;
+
+        if (/0[xX][0-9a-fA-F]+/.test(addr)) {
+            const transac = await contract.methods.getVoter(addr).call({ from: accounts[0] });
+            updateVotedId(transac.votedProposalId);
+            updateVoterAddress(addr);
+        } else {
+            alert("Not a valid address");
+        }
     }
 
     const handleChange = e => {
-        if (/^(0|[1-9][0-9]*)$/.test(e.target.value)) {
-            updateProposalId(e);
-        }
+        updateProposalId(e);
     }
-
-    const handleVoterChange = e => {
-        if (/0[xX][0-9a-fA-F]+/.test(e.target.value)) {
-          updateVoterAddress(e.target.value);
-        }
-    };
 
     return(
         <div>
             <input placeholder="set id of proposal" onChange={handleChange}></input>
             <button onClick={vote}>Vote</button>
             <br />
-            <input placeholder="search voter from address" onChange={handleVoterChange}></input>
+            <input id="address" placeholder="search voter from address"></input>
             <button onClick={getVoter}>Get Voter</button>
+            <p>Voter {voterAddress} voted for proposal {votedId} </p>
         </div>
     )
 }
