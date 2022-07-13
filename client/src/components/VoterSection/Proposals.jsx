f522a25324a88fe0f7cc746648cbdcdee601c649
Jean <jgatbkk@gmail.com>
2022-07-13 17:48:43 +0200

    handle onlyVoter

--- client/src/components/VoterSection/Proposals.jsx
+++ client/src/components/VoterSection/Proposals.jsx
@@ -1,42 +1,32 @@
-import { useEffect, useState } from "react";
-import useEth from "../../contexts/EthContext/useEth";
+import { useState } from "react";
 
-function Proposals() {
-  const { state: { contract, accounts } } = useEth();
-  const [proposals, setProposals] = useState([]);
+// props: number, updateNumber, setProposals, contract, accounts
+function Proposals(props) {
   const [description, setDescription] = useState("");
   const [voteCount, setVoteCount] = useState(0);
-  const [number, updateNumber] = useState(0);
   const [id, setId] = useState("");
-
-  useEffect(() => {
-    const first = async() => {
-      if(contract) {
-        const transaction = await contract.getPastEvents('ProposalRegistered', {fromBlock: 0});
-        const numberOfTransactions = transaction.length;
-        updateNumber(numberOfTransactions);
-        setProposals(transaction);
-      }
-    }
-    first();
-  }, [contract, accounts]);
 
   const addProposal = async() => {
     if(description !== "") {
-      const transac = await contract.methods.addProposal(description).send({ from: accounts[0] });
+      const transac = await props.contract.methods.addProposal(description).send({ from: props.accounts[0] });
       const eventData = transac.events.ProposalRegistered.returnValues._proposalId;
-      updateNumber(parseInt(eventData));
-      setProposals(proposals => [...proposals, transac.events.ProposalRegistered]);
+      props.updateNumber(parseInt(eventData));
+      props.setProposals(proposals => [...proposals, transac.events.ProposalRegistered]);
     } else {
       alert('Proposal description cannot be empty');
     }
   } 
 
   const getProposal = async() => {
-    if (/^(0|[1-9][0-9]*)$/.test(id)) { // Also check if Id exists ( < nb of proposals)
-      const proposal = await contract.methods.getOneProposal(id).call({ from: accounts[0] });
-      setVoteCount(proposal.voteCount);
-      // Do something with proposal -> show vote count 
+    if (/^(0|[1-9][0-9]*)$/.test(id)) {
+      if(id < props.number) {
+        const proposal = await props.contract.methods.getOneProposal(id).call({ from: props.accounts[0] });
+        setVoteCount(proposal.voteCount);
+        console.log("do something");
+        // Do something with proposal -> show vote count 
+      } else {
+        alert("Proposal associated to this id does not exist");
+      }
     } else {
       alert('Proposal must be a positive number');
     }
@@ -52,19 +42,13 @@ function Proposals() {
 
   return(
     <div>
-      <p>IDs start at 0 and latest proposal id is {number > 0? number-1: "None"}</p>
       <input placeholder="add proposal description" onChange={handleAddProposal}></input>
       <button onClick={addProposal}>press</button>
       <br />
       <input placeholder="get proposal by id" onChange={handleGetProposal}></input>
       <button onClick={getProposal}>press</button>
       <p>This proposal got {voteCount} votes</p>
-      <table>
-        <tbody>
-          {proposals.map((proposal) => 
-            (<tr key={proposal.id}><td>{proposal.returnValues._proposalId}</td><td>{proposal.returnValues._desc}</td></tr>))}
-        </tbody>
-      </table>
+
     </div>
   )
 }
--- client/src/components/VoterSection/index.jsx
+++ client/src/components/VoterSection/index.jsx
@@ -1,19 +1,69 @@
+import useEth from "../../contexts/EthContext/useEth";
 import Proposals from "./Proposals";
 import Vote from "./Vote";
+import { useState, useEffect } from "react";
 
-function VoterSection() {
-  const voters = 
-    <>
-      <Proposals/>
-      <br />
-      <br />
-      <Vote/>
-    </>; 
+function VoterSection( { addresses }) {
+  const { state: { contract, accounts } } = useEth();
+  const [proposals, setProposals] = useState([]);
+  const [number, updateNumber] = useState(0);
+  const [component, updateComponent] = useState();
+
+  useEffect(() => {
+    const onlyVoter = 
+      <>
+        <Proposals 
+          setProposals={setProposals} 
+          updateNumber={updateNumber}
+          number={number}
+          contract={contract}
+          accounts={accounts}/>
+        <br />
+        <Vote number={number}/>
+      </>; 
+
+    const loadProposals = async() => {
+      try {
+        const transaction = await contract.getPastEvents('ProposalRegistered', {fromBlock: 0});
+        const numberOfTransactions = transaction.length;
+        updateNumber(numberOfTransactions);
+        setProposals(transaction);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    function loadOnlyVoters() {
+      let isVoter = false;
+      addresses.forEach(voter => {
+        if(voter.returnValues._voterAddress === accounts[0]) isVoter = true;
+      });
+      isVoter === true ? updateComponent(onlyVoter) : updateComponent(<>You are not a voter</>);
+    }
+
+    if(contract) {
+      loadProposals();
+      loadOnlyVoters();
+    }
+  }, [contract, accounts, addresses, number]);
   
+  const showProposals =
+    <>
+      <p>IDs start at 0 and latest proposal id is {number > 0? number-1: "None"}</p>
+      <table>
+        <tbody>
+          {proposals.map((proposal) => 
+            (<tr key={proposal.id}><td>{proposal.returnValues._proposalId}</td><td>{proposal.returnValues._desc}</td></tr>))}
+        </tbody>
+      </table>
+    </>
+
   return (
     <div className="voter">
       <h1 className="title">Voters Section</h1>
-      {voters}
+      {showProposals}
+      <br/>
+      {component}
     </div>
   );
 }  
