377270d24f8b67b32eb46b3c12e8164d3e25b9e3
Jean <jgatbkk@gmail.com>
2022-07-16 12:18:51 +0200

    add showing winner

--- client/src/components/OwnerSection/StatusChange.jsx
+++ client/src/components/OwnerSection/StatusChange.jsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import { useEffect } from "react";
 import useEth from "../../contexts/EthContext/useEth";
 import "../../styling/Buttons.css";
 
 function StatusChange({ setStatus }) {
     const { state: { accounts, contract } } = useEth();
+    const [winner, setWinner] = useState();
+    const [showWinner, handleShowWinner] = useState(false);
 
     useEffect(() => {
         const loadStatus = async() => {
             const status = await contract.methods.workflowStatus().call();
             setStatus(status);
         }
+
+        const checkWinner = async() => {
+            const status = await contract.methods.workflowStatus().call();
+            if(parseInt(status) === 5) {
+                const winnerId = await contract.methods.winningProposalID().call();
+                setWinner(winnerId);
+                handleShowWinner(true);
+            }
+        }
+
         loadStatus();
+        checkWinner();
     }, [contract, setStatus]);
 
     const updateStatus = async() => {
@@ -65,7 +79,11 @@ function StatusChange({ setStatus }) {
     const tallyVotes = async() => {
         try {
             await contract.methods.tallyVotes().send({ from: accounts[0] });
+            const winner = await contract.methods.winningProposalID().call();
             updateStatus();
+            setWinner(winner);
+            handleShowWinner(true);
+            console.log(winner);
         } catch (error) {
             if(error.message.includes("Current status is not voting session ended")) {
                 alert("Current status is not voting session ended");
@@ -73,14 +91,20 @@ function StatusChange({ setStatus }) {
         }
     }
 
+    const winnerComponent =
+        <p>The winner of this voting session is proposal {winner}</p>
+
     return(
-        <div class="statusButtons">
-            <button class="buttonS" onClick={startProp}>Start proposal phase</button>
-            <button class="buttonS" onClick={endProp}>End proposal phase</button>
-            <button class="buttonS" onClick={startVoting}>Start voting phase</button>
-            <button class="buttonS" onClick={endVoting}>End voting phase</button>
-            <button class="buttonS" onClick={tallyVotes}>Tally Votes</button>
-        </div>
+        <>
+            {showWinner === true ? winnerComponent : null}
+            <div class="statusButtons">
+                <button class="buttonS" onClick={startProp}>Start proposal phase</button>
+                <button class="buttonS" onClick={endProp}>End proposal phase</button>
+                <button class="buttonS" onClick={startVoting}>Start voting phase</button>
+                <button class="buttonS" onClick={endVoting}>End voting phase</button>
+                <button class="buttonS" onClick={tallyVotes}>Tally Votes</button>
+            </div>
+        </>
     )
 }
 
